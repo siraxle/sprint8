7825fd7d5ef86eecd31cd9a6ca25437d83fc4f58
eefremov <axleevgeney@gmail.com>
2024-03-19 21:03:00 +0300

    add token

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
-    String bearerToken = "сюда_впиши_свой_токен";
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFkNWIzODQxNzBhNTAwM2Q0NDdlZGEiLCJpYXQiOjE3MTA4NzEyMzYsImV4cCI6MTcxMTQ3NjAzNn0._wY03WucsPGIOJg2Kunoql9uEV4ywbAiKOaLeHg85-k";
 
     @Before
     public void setUp() {
